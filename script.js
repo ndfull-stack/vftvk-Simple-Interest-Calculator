963e6cd393e3baa5e125ec6e623f275ca40c6677
Namandeep Singh <namanfullstackdev@gmail.com>
2021-08-28 23:53:29 +0530

    Modified the HTML, CSS and JS Files

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+function updateRate() 
+{
+    const rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval + "%";
+}
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+ 
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+    const interest = principal * years * rate / 100;
+    const year = new Date().getFullYear()+ parseInt(years);  
     
+    //Condition for the Negative, Zero or No Input in Amount field
+    if(parseInt(principal) <= 0 || !principal){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();  // Focus on Amount field 
+    }
+    else 
+    document.getElementById("result").innerHTML= "\<br>If you deposit $<mark>" +principal+"</mark>,\<br\>at an interest rate of <mark>" + rate +"%</mark>\<br\> You will receive an amount of <mark>$" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>";
 }
         
